ec36bc2e5c0cc959df2a6f898f598c226d124d66
Niels Kaars <nielskaars@gmail.com>
2025-02-27 14:02:50 +0100

    Basic Flask application without azure connection

--- backend/static/script.js
+++ backend/static/script.js
@@ -66,7 +66,7 @@ const copyResponse = (copyBtn) => {
 const showTypingAnimation = () => {
     const html = `<div class="chat-content">
                     <div class="chat-details">
-                        <img src="static/images/chatbot.jpg" alt="chatbot-img">
+                        <img src="static/images/chatbot.png" alt="chatbot-img">
                         <div class="typing-animation">
                             <div class="typing-dot" style="--delay: 0.2s"></div>
                             <div class="typing-dot" style="--delay: 0.3s"></div>
